fad290f3c6dd3c7ba38e4425a9b9bb1d8b39ea03
abcdefg_minSUng <vikavolk531@mail.ru>
2025-11-26 16:06:24 +0300

    1 страница (для мобильных версий работает)

--- lib/Pages_1.dart
+++ lib/Pages_1.dart
@@ -11,6 +11,8 @@ class PagesScreen extends StatefulWidget {
 class _PagesScreenState extends State<PagesScreen> {
   @override
   Widget build(BuildContext context) {
+    final isMobile = MediaQuery.of(context).size.width < 600;
+
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
@@ -20,89 +22,11 @@ class _PagesScreenState extends State<PagesScreen> {
       ),
 
       child: Scaffold(
+        drawer: _dialogMenu(context),
         backgroundColor: Colors.transparent,
         body: Stack(
           children: [
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                color: Color(0xFF3A2B28),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.asset(
-                      'assets/images/logo.png',
-                      width: 50,
-                      height: 50,
-                    ),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              'Главная',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Color.fromARGB(255, 255, 252, 231),
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 40),
-                          TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              'О нас',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Color.fromARGB(255, 255, 252, 231),
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 40),
-                          TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              'Поддержать',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Color.fromARGB(255, 255, 252, 231),
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 40),
-                          TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              'Меню',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Color.fromARGB(255, 255, 252, 231),
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 40),
-                          TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              'Контакты',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Color.fromARGB(255, 255, 252, 231),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            _buildHeader(context, isMobile),
 
             Center(
               child: Column(
@@ -111,49 +35,176 @@ class _PagesScreenState extends State<PagesScreen> {
                   Text(
                     'Утренний кот',
                     style: TextStyle(
-                      fontSize: 100,
+                      fontSize: MediaQuery.of(context).size.width > 600
+                          ? 100
+                          : 55,
                       color: Color.fromARGB(255, 255, 252, 231),
                     ),
                   ),
-                  SizedBox(height: 50),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width > 600 ? 50 : 20,
+                  ),
                   Container(
-                    padding: EdgeInsets.symmetric(horizontal: 100),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width > 600
+                          ? 100
+                          : 25,
+                    ),
                     alignment: Alignment.center,
                     child: Text(
                       'Стресс на работе? Погода не радует? Приходи в наше уютное кафе — здесь тебя ждут сладкие десерты, кофе и мурчащие коты!',
                       textAlign: TextAlign.center,
                       style: TextStyle(
-                        fontSize: 30,
+                        fontSize: MediaQuery.of(context).size.width > 600
+                            ? 30
+                            : 15,
                         color: Color.fromARGB(255, 255, 252, 231),
                       ),
                     ),
-                  
                   ),
-                  Padding(padding: EdgeInsets.all(70)),
+                  Padding(
+                    padding: EdgeInsets.all(
+                      MediaQuery.of(context).size.width > 600 ? 70 : 30,
+                    ),
+                  ),
                   Container(
                     child: ElevatedButton(
                       onPressed: () {
-                        Navigator.push(context, 
-                              MaterialPageRoute(
-                                builder: (BuildContext context) => BronScreen(),
-                              )
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => BronScreen(),
+                          ),
                         );
-                      }, 
-                      child: Text('ЗАБРОНИРОВАТЬ', style: TextStyle(color: Color(0xFF3A2B28))),
+                      },
+                      child: Text(
+                        'ЗАБРОНИРОВАТЬ',
+                        style: TextStyle(color: Color(0xFF3A2B28)),
+                      ),
                       style: ElevatedButton.styleFrom(
-                        textStyle: TextStyle(fontSize: 30),
+                        textStyle: TextStyle(
+                          fontSize: MediaQuery.of(context).size.width > 600
+                              ? 30
+                              : 16,
+                        ),
                         backgroundColor: Color.fromARGB(255, 255, 252, 231),
                         elevation: 5,
-                        padding: EdgeInsets.only(top: 20, bottom: 20, left: 40, right: 40),
-                      )
+                        padding: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width > 600
+                              ? 25
+                              : 10,
+                          vertical: MediaQuery.of(context).size.width > 600
+                              ? 30
+                              : 15,
+                        ),
+                      ),
                     ),
-
-                  )
+                  ),
                 ],
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildHeader(BuildContext context, bool isMobile) {
+    return Positioned(
+      top: 0,
+      left: 0,
+      right: 0,
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: isMobile ? 20 : 20,
+          vertical: isMobile ? 20 : 20,
+        ),
+        color: Color(0xFF3A2B28),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: isMobile ? 35 : 50,
+              height: isMobile ? 35 : 50,
+            ),
+
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (!isMobile) ...[
+                    _menuButton('Главная', isMobile),
+                    _menuButton('О нас', isMobile),
+                    _menuButton('Поддержать', isMobile),
+                    _menuButton('Меню', isMobile),
+                    _menuButton('Контакты', isMobile),
+                  ]
+                ],
+              ),
+            ),
+            if (isMobile)
+              Builder(
+                builder: (context) => IconButton(
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                  icon: Icon(Icons.menu, color: Color.fromARGB(255, 255, 252, 231)),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _menuButton(String title, bool isMobile) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: isMobile ? 10 : 20),
+      child: TextButton(
+        onPressed: () {},
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: isMobile ? 15 : 20,
+            color: Color.fromARGB(255, 255, 252, 231),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _dialogMenu(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(color: Color(0xFF3A2B28)),
+            child: Image.asset(
+              'assets/images/logo.png',
+              width: 100,
+              height: 100,
+            ),
+          ),
+          ListTile(
+            title: Text('Главная'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(title: Text('О нас'), onTap: () => Navigator.pop(context)),
+
+          ListTile(
+            title: Text('Поддержать'),
+            onTap: () => Navigator.pop(context),
+          ),
+          ListTile(title: Text('Меню'), onTap: () => Navigator.pop(context)),
+          ListTile(
+            title: Text('Контакты'),
+            onTap: () => Navigator.pop(context),
+          ),
+        ],
       ),
     );
   }
