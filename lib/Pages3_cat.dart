cdd8c9bd8f2ddde128030421567af5cf3a836ef7
abcdefg_minSUng <vikavolk531@mail.ru>
2025-11-29 09:57:15 +0300

    добавление бд в проект (бд рабочая и на сайте показываются коты)

--- lib/Pages3_cat.dart
+++ lib/Pages3_cat.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cafe/Pages_1.dart';
 import 'SiteHeader.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 const double KHeaderHeight = 80.0;
+
 class CatScreen extends StatefulWidget {
   const CatScreen({super.key});
 
@@ -13,6 +14,42 @@ class CatScreen extends StatefulWidget {
 
 class _CatScreenState extends State<CatScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  List<Map<String, dynamic>> cats = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCatsFromFirestore();
+  }
+
+  Future<void> _loadCatsFromFirestore() async {
+    try {
+      final snapshot = await FirebaseFirestore.instance
+          .collection('cats')
+          .get();
+
+      setState(() {
+        cats = snapshot.docs.map((doc) {
+          final data = doc.data();
+          print('Загружен кот: ${data['name']} - ${data['imageUrl']}');
+          return {
+            'name': data['name'] as String,
+            'description': data['description'] as String,
+            'gender': data['gender'] as String,
+            'imageUrl': data['imageUrl'] as String,
+          };
+        }).toList();
+        isLoading = false;
+      });
+    } catch (e) {
+      print('Ошибка загрузки котов: $e');
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,30 +85,6 @@ class _CatScreenState extends State<CatScreen> {
                 final isMobile = MediaQuery.of(context).size.width < 600;
                 final horizontalPadding = isMobile ? 24.0 : 120.0;
 
-                final catNames = [
-                  'Дин',
-                  'Кас',
-                  'Сэм',
-                  'Дин',
-                  'Кас',
-                  'Сэм',
-                  'Дин',
-                  'Кас',
-                  'Сэм',
-                ];
-
-                final catImages = [
-                  'assets/images/cat/DIN.png',
-                  'assets/images/cat/KAS.png',
-                  'assets/images/cat/SAM.png',
-                  'assets/images/cat/DIN.png',
-                  'assets/images/cat/KAS.png',
-                  'assets/images/cat/SAM.png',
-                  'assets/images/cat/DIN.png',
-                  'assets/images/cat/KAS.png',
-                  'assets/images/cat/SAM.png',
-                ];
-
                 return Padding(
                   padding: EdgeInsets.only(
                     top: KHeaderHeight + 80,
@@ -80,97 +93,181 @@ class _CatScreenState extends State<CatScreen> {
                     bottom: 20,
                   ),
 
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Наши котики',
-                          style: TextStyle(
-                            fontSize: isMobile ? 20 : 45,
-                            color: Color(0xFF3A2B28),
+                  child: isLoading
+                      ? Center(child: CircularProgressIndicator())
+                      : SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Наши котики',
+                                style: TextStyle(
+                                  fontSize: isMobile ? 20 : 45,
+                                  color: Color(0xFF3A2B28),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 40,
+                              ), // вертикальный отступ (не width!)
+                              // Сетка котиков
+                              LayoutBuilder(
+                                builder: (context, constraints) {
+                                  final screenWidth = constraints.maxWidth;
+                                  final crossAxisCount = screenWidth < 600
+                                      ? 1
+                                      : 6;
+                                  final itemSpacing = isMobile ? 20.0 : 30.0;
+                                  final itemSize =
+                                      (screenWidth -
+                                          (crossAxisCount - 1) * itemSpacing) /
+                                      crossAxisCount;
+
+                                  return GridView.builder(
+                                    itemCount: cats.length,
+                                    shrinkWrap: true,
+                                    physics:
+                                        const NeverScrollableScrollPhysics(),
+                                    gridDelegate:
+                                        SliverGridDelegateWithFixedCrossAxisCount(
+                                          crossAxisCount: crossAxisCount,
+                                          crossAxisSpacing: itemSpacing,
+                                          mainAxisSpacing: itemSpacing * 1.2,
+                                          mainAxisExtent: itemSize + 60,
+                                        ),
+                                    itemBuilder: (context, index) {
+                                      final cat = cats[index];
+                                      return GestureDetector(
+                                        onTap: () {
+                                          _showCatDetails(context, cat);
+                                        },
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Container(
+                                              width: itemSize,
+                                              height: itemSize,
+                                              decoration: BoxDecoration(
+                                                color: const Color.fromARGB(
+                                                  0,
+                                                  255,
+                                                  255,
+                                                  255,
+                                                ).withOpacity(0.85),
+                                                borderRadius:
+                                                    BorderRadius.circular(14),
+                                                boxShadow: [
+                                                  BoxShadow(
+                                                    color: Colors.black
+                                                        .withOpacity(0.1),
+                                                    blurRadius: 6,
+                                                    spreadRadius: 1,
+                                                  ),
+                                                ],
+                                              ),
+                                              child: ClipRRect(
+                                                borderRadius:
+                                                    BorderRadius.circular(14),
+                                                child: Image.asset(
+                                                  cat['imageUrl'],
+                                                  fit: BoxFit.cover,
+                                                ),
+                                              ),
+                                            ),
+                                            const SizedBox(height: 8),
+                                            Text(
+                                              cat['name'],
+                                              style: const TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold,
+                                                color: Color(0xFF3A2B28),
+                                              ),
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ],
+                                        ),
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
+                            ],
                           ),
                         ),
-                        const SizedBox(
-                          height: 40,
-                        ), // вертикальный отступ (не width!)
-                        // Сетка котиков
-                        LayoutBuilder(
-                          builder: (context, constraints) {
-                            final screenWidth = constraints.maxWidth;
-                            final crossAxisCount = screenWidth < 600 ? 1 : 6;
-                            final itemSpacing = isMobile ? 20.0 : 30.0;
-                            final itemSize =
-                                (screenWidth -
-                                    (crossAxisCount - 1) * itemSpacing) /
-                                crossAxisCount;
-
-                            return GridView.builder(
-                              itemCount: catNames.length,
-                              shrinkWrap: true,
-                              physics: const NeverScrollableScrollPhysics(),
-                              gridDelegate:
-                                  SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: crossAxisCount,
-                                    crossAxisSpacing: itemSpacing,
-                                    mainAxisSpacing: itemSpacing * 1.2,
-                                    mainAxisExtent: itemSize + 60,
-                                  ),
-                              itemBuilder: (context, index) {
-                                return Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Container(
-                                      width: itemSize,
-                                      height: itemSize,
-                                      decoration: BoxDecoration(
-                                        color: const Color.fromARGB(0, 255, 255, 255).withOpacity(0.85),
-                                        borderRadius: BorderRadius.circular(14),
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color: Colors.black.withOpacity(
-                                              0.1,
-                                            ),
-                                            blurRadius: 6,
-                                            spreadRadius: 1,
-                                          ),
-                                        ],
-                                      ),
-                                      child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(14),
-                                        child: Image.asset(
-                                          catImages[index],
-                                          // width: screenWidth < 600 ? 150 : 200,
-                                          // height: screenWidth < 600 ? 150 : 200,
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                    ),
-                                    const SizedBox(height: 8),
-                                    Text(
-                                      catNames[index],
-                                      style: const TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                        color: Color(0xFF3A2B28),
-                                      ),
-                                      textAlign: TextAlign.center,
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
                 );
               },
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void _showCatDetails(BuildContext context, Map<String, dynamic> cat) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // Картинка кота
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    cat['imageUrl'],
+                    width: 120,
+                    height: 120,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                // Имя
+                Text(
+                  cat['name'],
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF3A2B28),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                // Пол
+                Text(
+                  'Пол: ${cat['gender']}',
+                  style: const TextStyle(fontSize: 16, color: Colors.grey),
+                ),
+                const SizedBox(height: 8),
+                // Описание
+                Text(
+                  cat['description'],
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 15, height: 1.5),
+                ),
+                const SizedBox(height: 20),
+                // Кнопка "Закрыть"
+                ElevatedButton(
+                  onPressed: () => Navigator.pop(context),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF3A2B28),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    'Закрыть',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 
